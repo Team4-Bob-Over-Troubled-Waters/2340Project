3a62564d787c415d4219215f392db6857ad74790
francineluo <francine_luo@yahoo.com>
2018-02-16 14:38:09 -0500

    added shelter listing

--- app/src/main/java/cs2340/bob_over_troubled_waters/homelessshelterapplication/controller/UserHome.java
+++ app/src/main/java/cs2340/bob_over_troubled_waters/homelessshelterapplication/controller/UserHome.java
@@ -4,14 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import cs2340.bob_over_troubled_waters.homelessshelterapplication.R;
+import cs2340.bob_over_troubled_waters.homelessshelterapplication.model.Shelter;
 import cs2340.bob_over_troubled_waters.homelessshelterapplication.model.User;
 
 public class UserHome extends AppCompatActivity {
 
     private User user;
+    private ArrayList<Shelter> shelters = new ArrayList<>();
+    private ListView shelterListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +45,30 @@ public class UserHome extends AppCompatActivity {
         Intent intent = new Intent(this, WelcomeScreen.class);
         startActivity(intent);
         finish();
+    }
+
+    public void loadShelterAction(View view) {
+        try (
+            InputStream inputStream = getResources().openRawResource(R.raw.homeless_shelter_database);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build()
+        ) {
+            String[] nextShelter;
+            System.out.println("File was found.");
+            while ((nextShelter = csvReader.readNext()) != null) {
+                shelters.add(new Shelter(nextShelter[0], nextShelter[1], nextShelter[2],
+                        nextShelter[3], nextShelter[4], nextShelter[5], nextShelter[6],
+                        nextShelter[7], nextShelter[8]));
+            }
+            System.out.println("File has been parsed.");
+            setContentView(R.layout.activity_shelter_listing);
+            shelterListView = (ListView) findViewById(R.id.shelter_listing);
+            ArrayAdapter<Shelter> arrayAdapter = new ArrayAdapter<Shelter>(this,
+                    android.R.layout.simple_list_item_1, shelters);
+            shelterListView.setAdapter(arrayAdapter);
+            System.out.println("List has been updated.");
+        } catch (IOException e) {
+            System.out.println("There was an error loading shelter information.");
+        }
     }
 }
---
+++ app/src/main/java/cs2340/bob_over_troubled_waters/homelessshelterapplication/model/Shelter.java
@@ -0,0 +1,107 @@
+package cs2340.bob_over_troubled_waters.homelessshelterapplication.model;
+
+/**
+ * Created by Francine on 2/16/2018.
+ */
+
+public class Shelter {
+    private int ID;
+    private String name;
+    private String capacity;
+    private String restrictions;
+    private double longitude;
+    private double latitude;
+    private String address;
+    private String specialNotes;
+    private String phoneNumber;
+
+    public Shelter(String ID, String name, String capacity, String restrictions, String longitude,
+                   String latitude, String address, String specialNotes, String phoneNumber) {
+        this.ID = Integer.parseInt(ID);
+        this.name = name;
+        this.capacity = capacity;
+        this.restrictions = restrictions;
+        this.longitude = Double.parseDouble(longitude);
+        this.latitude = Double.parseDouble(latitude);
+        this.address = address;
+        this.specialNotes = specialNotes;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getRestrictions() {
+        return restrictions;
+    }
+
+    public void setRestrictions(String restrictions) {
+        this.restrictions = restrictions;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSpecialNotes() {
+        return specialNotes;
+    }
+
+    public void setSpecialNotes(String specialNotes) {
+        this.specialNotes = specialNotes;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

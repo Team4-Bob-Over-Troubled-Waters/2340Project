751e0df6eab75fe8737d25cd744f99fb1dc79935
francineluo <francine_luo@yahoo.com>
2018-03-28 18:30:36 -0400

    added infowindows and view map button

--- app/src/main/java/cs2340/bob_over_troubled_waters/homelessshelterapplication/controller/UserHome.java
+++ app/src/main/java/cs2340/bob_over_troubled_waters/homelessshelterapplication/controller/UserHome.java
@@ -50,4 +50,9 @@ public class UserHome extends AppCompatActivity {
         Intent intent = new Intent(this, ReservationPage.class);
         startActivity(intent);
     }
+
+    public void viewMapButtonAction(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }

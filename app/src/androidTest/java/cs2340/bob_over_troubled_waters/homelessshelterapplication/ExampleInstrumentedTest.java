a226504ae089139fe5af95776a527cbb457c98c5
sem42198 <sem42198@gmail.com>
2018-04-11 13:26:38 -0400

    fixed my merge mistakes

--- app/src/androidTest/java/cs2340/bob_over_troubled_waters/homelessshelterapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cs2340/bob_over_troubled_waters/homelessshelterapplication/ExampleInstrumentedTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,7 +17,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
+    public static void sampleTest() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
